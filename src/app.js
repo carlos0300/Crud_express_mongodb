1350ce8bfe44bae6b5afb810f23286b8fc17aca2
carlos0300 <carlosbecerra0300@gmail.com>
2021-12-09 16:18:48 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const init_db = require('../config/db')
+//const init_db = require('../config/db')
 
 //acá debemos pasar las rutas
 const routes = require('./Routes/index_routes')
@@ -16,4 +16,4 @@ app.listen(3000, () => {
     console.log('servidor a la espera de conexiones')
 })
 
-init_db()
+//init_db()
